a583270dca0edf23f37474c79d1026dee5e59a1d
Raphael <raphaellaxz@yahoo.com.hk>
2019-07-11 22:11:13 +0800

    commit all soruce

--- src/main/java/com/packet/first/App.java
+++ src/main/java/com/packet/first/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
+        System.out.println( "this is my first maven on docker, yoyo ;>)");
     }
 }
